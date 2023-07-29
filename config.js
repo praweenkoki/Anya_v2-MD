6086e25ec70409a0d8dca3414d1d596aadfc3f68
praweenkoki <140908078+praweenkoki@users.noreply.github.com>
2023-07-30 03:38:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ global.lApiKey = {// if this API key get expired then please go to https://api.l
 
 
 global.botname = "Queen Anya" 
-global.author = "Enter Author Name Here"  
+global.author = "ᴍʀ ᴘʀᴀᴡᴇᴇɴ"  
 global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
 global.myweb = "Enter Your Any Website Profile Link Here"
 global.footer = "©\t" + "Enter Your Watermark Here"
@@ -34,8 +34,8 @@ global.socialText = `*⪧⪢ Please join this group for by bot's announcement.*`
 global.socialLink = `*⪧⪢ https://chat.whatsapp.com/HshHS6kHF9NHnA9lfMwwSM 💝*`
 
 
-global.ownername = "Enter Owner Name Here"    
-global.ownernumber = ["918811074852"]
+global.ownername = "ᴍʀ ᴘʀᴀᴡᴇᴇɴ"    
+global.ownernumber = ["94753671279"]
 global.ytname = "YT: Enter Your Channel Name Here"
 global.continent = "Asia" // your continent name 
 global.region = "Enter Your Country Name Here"
